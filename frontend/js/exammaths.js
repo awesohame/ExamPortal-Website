5a18f8e920a3b12d28f5cd26667030345c26945d
Sanctum07 <82087247+Sanctum07@users.noreply.github.com>
2023-10-17 21:21:46 +0530

    First Upload

---
+++ frontend/js/exammaths.js
@@ -0,0 +1,210 @@
+const questionArr = [
+    {
+        question: "What is the sum of all prime numbers between 20 and 30?",
+        a: "73",
+        b: "80",
+        c: "79",
+        d: "67",
+        correct: "c"
+    },
+
+    {
+        question: "If a quadratic equation 2x² - 5x + k = 0 has equal roots, what is the value of 'k'?",
+        a: "5/2",
+        b: "25/4",
+        c: "11",
+        d: "18",
+        correct: "b"
+    },
+
+    {
+        question: "If f(x) = x³ - 4x² + 3x + 2, what is f'(x) (the derivative of f(x))?",
+        a: "3x² - 8x + 3",
+        b: "2x² - 8x + 3",
+        c: "3x² - 5x + 3",
+        d: "8x² - 8x + 9",
+        correct: "a"
+    },
+
+    {
+        question: "If the equation of a circle is (x - 2)² + (y + 3)² = 25, what is the radius of the circle?",
+        a: "5",
+        b: "4",
+        c: "9",
+        d: "16",
+        correct: "a"
+    },
+
+    {
+        question: "Solve the trigonometric equation: 2sin(x) - √3 = 0 for 0° ≤ x < 360°. What are the solutions?",
+        a: "60°, 300°",
+        b: "45°, 225°",
+        c: "30°, 100°",
+        d: "30°, 60°",
+        correct: "a"
+
+    },
+    {
+        question: "A triangle has sides of length 6 cm, 8 cm, and 10 cm. Is it a right-angled triangle?",
+        a: "Yes",
+        b: "No",
+        c: "Cannot determine",
+        d: "Maybe",
+        correct: "a"
+
+    },
+    {
+        question: "If log base 3 of x = 2, what is the value of x?",
+        a: "3",
+        b: "6",
+        c: "9",
+        d: "1",
+        correct: "c"
+
+    },
+    {
+        question: "What is the value of the expression (1 - i)², where 'i' is the imaginary unit?",
+        a: "2 - 2i",
+        b: "8 + i",
+        c: "8 - i",
+        d: "-3",
+        correct: "a"
+
+    },
+    {
+        question: "If a fair six-sided die is rolled, what is the probability of rolling a prime number?",
+        a: "8",
+        b: "1/2",
+        c: "1/6",
+        d: "7",
+        correct: "c"
+
+    },
+    {
+        question: "A right circular cone has a radius of 4 cm and a height of 9 cm. What is the volume of the cone?",
+        a: "12π cm³",
+        b: "90π cm³",
+        c: "108π cm³",
+        d: "18π cm³",
+        correct: "c"
+
+    },
+];
+
+const quiz = document.getElementById('quiz');
+const choicesEls = document.querySelectorAll('.choices');
+const questionEls = document.getElementById('question');
+const a_data = document.getElementById('a_data');
+const b_data = document.getElementById('b_data');
+const c_data = document.getElementById('c_data');
+const d_data = document.getElementById('d_data');
+const a_radio = document.getElementById('a');
+const b_radio = document.getElementById('b');
+const c_radio = document.getElementById('c');
+const d_radio = document.getElementById('d');
+const submitBtn = document.getElementById('myBtn');
+const quesbtn = document.querySelectorAll('.ques');
+const finishbtn = document.getElementById('finish');
+let choices = ['e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e'];
+
+let currentQuiz = 0;
+let score = 0;
+
+loadQuiz();
+
+function loadQuiz() {
+    deselectChoices();
+
+    const currentQuizData = questionArr[currentQuiz];
+
+    questionEls.innerText = currentQuizData.question;
+    a_data.innerText = currentQuizData.a;
+    b_data.innerText = currentQuizData.b;
+    c_data.innerText = currentQuizData.c;
+    d_data.innerText = currentQuizData.d;
+    // console.log(choices[currentQuiz])
+    if (choices[currentQuiz] != 'e') {
+        if (choices[currentQuiz] == 'a') {
+            a_radio.checked = true;
+        }
+        else if (choices[currentQuiz] == 'b') {
+            b_radio.checked = true;
+        }
+        else if (choices[currentQuiz] == 'c') {
+            c_radio.checked = true;
+        }
+        else if (choices[currentQuiz] == 'd') {
+            d_radio.checked = true;
+        }
+    }
+};
+//this function deselect the buttons
+function deselectChoices() {
+    choicesEls.forEach(choicesEl => choicesEl.checked = false);
+};
+//this function will get the selected buttos when a user clicks one.
+function getSelected() {
+    let choice;
+    choicesEls.forEach(choicesEls => {
+        if (choicesEls.checked) {
+            choice = choicesEls.id;
+        }
+    });
+    return choice;
+};
+//this method will add an event listener once the submit button has been clicked
+submitBtn.addEventListener('click', () => {
+    const chosen = getSelected();
+    // if (chosen) {
+    //     if (chosen === questionArr[currentQuiz].correct) {
+    //         score++;
+    //     };
+    if (chosen) {
+        choices[currentQuiz] = chosen;
+
+        // console.log(choices[currentQuiz]);
+    }
+    currentQuiz++;
+
+    if (currentQuiz <= questionArr.length) {
+        loadQuiz();
+    }
+    // else {
+    //     quiz.innerHTML = `
+    //     <h1 style="text-align: center">You scored ${score}/${questionArr.length}</h1>
+    //     <button onclick = "location.reload()" class="next_btn">Reload</button>
+    //     `
+    // }
+
+
+});
+
+quesbtn.forEach(item => {
+    item.addEventListener('click', () => {
+        //handle click
+        // console.dir(quesbtn[0]);
+        let index = (item.innerHTML) - 1;
+        currentQuiz = index;
+        // console.log(choices[currentQuiz]);
+        if (currentQuiz <= questionArr.length) {
+            loadQuiz();
+        }
+
+    })
+});
+
+finishbtn.addEventListener('click', () => {
+    // console.dir(finishbtn);
+    // console.log(choices.length)
+    for (let i = 0; i < choices.length; i++) {
+        // console.log(questionArr[i].correct)
+        if (choices[i] == questionArr[i].correct) {
+            score++;
+        }
+    }
+    quiz.innerHTML = `
+            <h1 style="text-align: center">You scored ${score}/${questionArr.length}</h1>
+            <button onclick = "location.reload()" class="next_btn">Reload</button>
+            `
+
+})
---
+++ index.js
@@ -0,0 +1,178 @@
+const express = require('express')
+const app = express();
+const path = require('path');
+const port = 80;
+const mysql = require('mysql2');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'users',
+    password: 'mysqlpw'
+});
+
+
+// try {
+//     let q = `SELECT * FROM user`
+//     connection.query(q, (err, results) => {
+//         if (err) throw err;
+
+//         console.log(results);
+//     }
+//     );
+// } catch (err) {
+//     console.log(err);
+// }
+
+// connection.end();
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/frontend/views"));
+
+app.use(express.static('frontend'));
+
+app.get('/', function (req, res) {
+    // res.render("./frontend/html/home.html")
+    res.sendFile(path.join(__dirname, '/frontend/html/home.html'));
+})
+
+app.get('/login', function (req, res) {
+    // res.render("./frontend/html/home.html")
+    res.sendFile(path.join(__dirname, '/frontend/html/login.html'));
+})
+
+app.get('/signup', function (req, res) {
+    // res.render("./frontend/html/home.html")
+    res.sendFile(path.join(__dirname, '/frontend/html/signup.html'));
+})
+
+// app.get('/continue', function (req, res) {
+//     // res.render("./frontend/html/home.html"
+//     let username = "test1";
+//     // res.send("ok")
+//     res.render('continue.ejs', { username });
+// })
+// function redir(req, res) {
+//     console.log(req.dataProcessed);
+// }
+// app.get('/continue', redir)
+
+app.post('/login', function (req, res) {
+    let username = req.body.username;
+    let password = req.body.password;
+
+    // console.log(req.body);
+    try {
+        let q = `SELECT * FROM user WHERE username = ? AND pass = ?`
+        connection.query(q, [username, password], (err, results) => {
+            if (err) throw err;
+            if (results.length > 0) {
+                // res.redirect('/continue');
+                res.render('continue.ejs', { username });
+            } else {
+                res.send('Incorrect Username and/or Password!');
+            }
+            //console.log(results);
+        }
+        );
+    } catch (err) {
+        console.log(err);
+        res.send("ERROR in db")
+    }
+    // res.send(`Welcome ${username}. pw is ${password}`);
+})
+
+app.post('/signup', function (req, res) {
+    let username = req.body.username;
+    let password = req.body.password;
+
+    // console.log(req.body);
+    try {
+        let q = `SELECT * FROM user WHERE username = ?`
+        connection.query(q, [username], (err, results) => {
+            if (err) throw err;
+            if (results.length != 0) {
+                // res.redirect('/continue');
+                // res.render('continue.ejs', { username });
+
+                res.send('test exists');
+            } else {
+
+                try {
+                    let newq = `INSERT INTO user (username,pass) VALUES ("${username}","${password}")`;
+                    connection.query(newq, [username, password], (err, results) => {
+                        if (err) throw err;
+
+                        // res.redirect('/continue');
+                        // res.render('continue.ejs', { username });
+                        console.log(results);
+                        res.redirect("/login");
+                    }
+                    );
+                } catch (err) {
+                    console.log(err);
+                    res.send("ERROR in db");
+
+                }
+            }
+
+            console.log(results);
+
+        }
+        );
+    } catch (err) {
+        console.log(err);
+        res.send("ERROR in db");
+    }
+
+    // try {
+    //     let newq = `INSERT INTO user (username,pass) VALUES ("${username}","${password}")`;
+    //     connection.query(newq, [username, password], (err, results) => {
+    //         if (err) throw err;
+
+    //         // res.redirect('/continue');
+    //         // res.render('continue.ejs', { username });
+    //         console.log(results);
+    //         res.redirect("/login");
+    //     }
+    //     );
+    // } catch (err) {
+    //     console.log(err);
+    //     res.send("ERROR in db");
+
+    // }
+
+    // res.send(`Welcome ${username}. pw is ${password}`);
+})
+
+app.post('/exam/:user', function (req, res) {
+    let user = req.params.user;
+    res.render("exam.ejs", { user });
+    // res.sendFile(path.join(__dirname, '/frontend/html/exam.html'));
+})
+
+app.post('/examphy/:user', function (req, res) {
+    let user = req.params.user;
+    res.render("examphy.ejs", { username });
+    // res.sendFile(path.join(__dirname, '/frontend/html/exam.html'));
+})
+
+app.post('/examchem/:user', function (req, res) {
+    let user = req.params.user;
+    res.render("examchem.ejs", { user });
+    // res.sendFile(path.join(__dirname, '/frontend/html/exam.html'));
+})
+
+app.post('/exammaths/:user', function (req, res) {
+    let user = req.params.user;
+    res.render("exammaths.ejs", { user });
+    // res.sendFile(path.join(__dirname, '/frontend/html/exam.html'));
+})
+// req.params
+
+app.listen(port, () => {
+    console.log('app is listening');
+});
